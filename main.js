9d393ada158951a9655194b999b8e51e2c4b46a3
johnny1864 <johnny1864@yahoo.com>
2018-12-29 20:07:31 -0800

    added styling and GET api call

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 "use strict";
 
+const API_KEY = 'b8dfeca82fc345c4a9c29bfa70176a8a';
+const newsURL = 'https://newsapi.org/v2/';
+
 window.addEventListener('load', showHeadlines);
 
 function showHeadlines() {
+    const spinner = document.getElementById('ajax-wait')
+    const headlinesUrl = `${newsURL}top-headlines?country=us&apiKey=${API_KEY}`;
     
+    // XHR REQUEST
+    const xhr = new XMLHttpRequest(); 
+    xhr.open("GET", headlinesUrl, true);
+    
+    // GET HEADLINE ON SUCCESSFUL CONNECTION
+    xhr.onload = function() {
+        document.getElementById('ajax-wait').style.display = 'none';
+        
+        if(xhr.status === 200 ){
+            var newsData = JSON.parse(xhr.responseText);
+            
+            for(let i = 0; i<newsData.articles.length; i++){
+                console.log(newsData.articles[i].title);
+            }
+        }
+        
+    };
+    
+    // 
+    xhr.onerror = function() {
+        spinner.style.display = 'none';
+    }
+    
+    xhr.send();
+    
+    spinner.style.display = 'block';
 }
