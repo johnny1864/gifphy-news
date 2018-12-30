587e082a4e6069861e89c60402b341c4b5522ee8
johnny1864 <johnny1864@yahoo.com>
2018-12-30 14:00:28 -0800

    created showGifs function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 "use strict";
 
+// API KEYS AND BASE URLs
 const API_KEY = 'b8dfeca82fc345c4a9c29bfa70176a8a';
+const giphy_key = 'SkNbmoGTSoszOKoRZYer7zO1PI2VUXRA';
 const newsURL = 'https://newsapi.org/v2/';
+const giphyURL = 'http://api.giphy.com/v1/gifs/'
 
 window.addEventListener('load', showHeadlines);
 
@@ -21,7 +24,7 @@ function showHeadlines() {
             var newsData = JSON.parse(xhr.responseText);
             
             for(let i = 0; i<newsData.articles.length; i++){
-                headlines.push('<div class="headline"> <a href="#article" class="headline-link" >');
+                headlines.push(`<div class="headline"> <a href="#article" class="headline-link" onclick="showGifs('${newsData.articles[i].title}')">`);
                 headlines.push(newsData.articles[i].title);
                 headlines.push('</a></div>');
             }
@@ -43,3 +46,35 @@ function showHeadlines() {
     
     spinner.style.display = 'block';
 }
+
+function showGifs(title) {
+    if(!title) title = 'batman';
+    console.log('headline is ',title);
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", `${giphyURL}search?q=${title}&api_key=${giphy_key}&limit=5`);
+    
+    xhr.onload = function() {
+        if(xhr.status === 200){
+            const gif_data = JSON.parse(xhr.responseText);
+            console.log(gif_data['data']);
+            //document.getElementById('gifs').innerHTML = ''
+        }
+    }
+    
+    xhr.onerror = function() {
+        const errorText = document.getElementById('error');
+        
+        errorText.innerHTML = "Unable to get your GIF, Please try again later."
+    }
+    
+    xhr.send();
+    
+}
+
+
+
+
+
+
+
